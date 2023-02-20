b2e0d33b19a385f265b5019152db899355734bc6
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-20 19:58:43 +0200

    add parallax when scroll

--- src/app/about.js
+++ src/app/about.js
@@ -48,5 +48,28 @@ window.onload = function () {
       coordXprocent = (coordX / parallaxWidth) * 100;
       coordYprocent = (coordY / parallaxHeight) * 100;
     });
+
+    // Parallax при скролле
+
+    let thresholdSets = [];
+    for (let i = 0; i <= 1.0; i += 0.005) {
+      thresholdSets.push(i);
+    }
+    const callback = function (entries, observer) {
+      const scrollTopProcent =
+        (window.pageYOffset / parallax.offsetHeight) * 100;
+      setParallaxItemsStyle(scrollTopProcent);
+    };
+    const observer = new IntersectionObserver(callback, {
+      threshold: thresholdSets,
+    });
+
+    observer.observe(document.querySelector('.content'));
+
+    function setParallaxItemsStyle(scrollTopProcent) {
+      content.style.cssText = `transform: translate(0%,-${
+        scrollTopProcent / 9
+      }%);`;
+    }
   }
 };
